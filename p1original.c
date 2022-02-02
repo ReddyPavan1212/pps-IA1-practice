69f50e9d4095e5a232eb5333410facf2c7a254fc
P.BABU REDDY <94963098+ReddyPavan1212@users.noreply.github.com>
2022-02-02 14:38:08 +0530

    Update p1original.c

--- p1original.c
+++ p1original.c
@@ -1,2 +1,11 @@
+#include <stdio.h>
 
+int main()
+
+{
+    int a,b, sum;
+    printf("Enter the value of numbers to be added\n");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("Sum of %d and %d is %d",a,b,sum);
 }
